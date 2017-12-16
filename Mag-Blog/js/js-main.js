471491dbc5268f8116d1b26d90d8572626a0fd52
AndriyStechenko <stechenkoao@gmail.com>
2017-12-16 13:18:04 +0200

    first commit
    first commit

---
+++ Mag-Blog/js/js-main.js
@@ -0,0 +1,16 @@
+/**
+ * Created by ravlik on 20.06.17.
+ */
+
+/** carousel*/
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        nav:true,
+        navText:false,
+        loop:true,
+        margin: 10,
+        items: 3
+    });
+
+});
